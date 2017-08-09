8cb133c403d725d2435f444f5fd43aa85d3dcf33
snwfdhmp <snwfdhmp@protonmail.com>
2017-08-09 16:32:26 +0200

    added sauces, meats, cheeses and improved README

--- README.md
+++ README.md
@@ -4,14 +4,39 @@
 
 `curl localhost:9876/yourSauce/yourMeat/yourCheese`
 
+If you want one of your pizza's part to be skipped, just say "no"
+
+Examples :
+
+Pizza with only ketchup and emmental
+
+`curl localhost:9876/ketchup/no/emmental`
+
+Pizza with buffalo sauce, bacon and mozzarella
+
+`curl localhost:9876/buffalo/bacon/mozzarella`
+
 Available sauces:
 
 	- tomato
+	- ketchup
+	- classico
+	- italian
+	- buffalo
+	- supreme
 
 Available meats:
 
 	- chicken
+	- bacon
 
 Available cheeses :
 
 	- goat
+	- mozzarella
+	- emmental
+	- ricotta
+	- provolone
+	- cheddar
+	- romano
+
--- cmd/pizzaman/pizzaman.go
+++ cmd/pizzaman/pizzaman.go
@@ -8,31 +8,65 @@ import (
 	"net/http"
 )
 
+var (
+	sauces  []string
+	meats   []string
+	cheeses []string
+)
+
 func main() {
 	log.Println("Welcome to your pizzeria")
-
-	var sauces []string
-	var meats []string
-	var cheeses []string
-
-	sauces = append(sauces, "tomato")
-	sauces = append(sauces, "ketchup")
 
 	meats = append(meats, "chicken")
 
 	cheeses = append(cheeses, "goat")
 
+	log.Println("Buying sauces")
+	buySauces()
+
+	log.Println("Buying meats")
+	buyMeats()
+
+	log.Println("Buying cheeses")
+	buyCheeses()
+
+	log.Println("Filling fridge")
 	fridge.FillSauces(sauces)
 	fridge.FillMeats(meats)
 	fridge.FillCheeses(cheeses)
 
-	log.Println("Fridge filled")
-
+	log.Println("Switching on phone")
 	router := routes.Init()
-	log.Println("Phone switched on")
 
+	log.Println("Waking up employees")
 	go orders.PrepareAll()
-	log.Println("Ready to prepare orders")
 
 	log.Fatal(http.ListenAndServe(":9876", router))
+}
+
+func buySauces() {
+	sauces = append(sauces, "tomato")
+	sauces = append(sauces, "ketchup")
+	sauces = append(sauces, "classico")
+	sauces = append(sauces, "italian")
+	sauces = append(sauces, "buffalo")
+	sauces = append(sauces, "supreme")
+	sauces = append(sauces, "no")
+}
+
+func buyMeats() {
+	meats = append(meats, "chicken")
+	meats = append(meats, "bacon")
+	meats = append(meats, "no")
+}
+
+func buyCheeses() {
+	cheeses = append(cheeses, "goat")
+	cheeses = append(cheeses, "mozzarella")
+	cheeses = append(cheeses, "emmental")
+	cheeses = append(cheeses, "ricotta")
+	cheeses = append(cheeses, "provolone")
+	cheeses = append(cheeses, "cheddar")
+	cheeses = append(cheeses, "romano")
+	cheeses = append(cheeses, "no")
 }
